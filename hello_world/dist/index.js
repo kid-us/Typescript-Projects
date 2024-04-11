ff7f623c3092f4b78a579277b1d0293932fc4730
kid-us <kidushh29@gmail.com>
2024-04-11 10:59:21 +0300

    Debugging

--- hello_world/dist/index.js
+++ hello_world/dist/index.js
@@ -1,3 +1,5 @@
 "use strict";
-console.log("Hello World");
 let age = 20;
+if (age < 50)
+    age += 10;
+//# sourceMappingURL=index.js.map
--- hello_world/src/index.ts
+++ hello_world/src/index.ts
@@ -1,2 +1,4 @@
-console.log("Hello World");
 let age: number = 20;
+if (age < 50)
+    age+=10;
+console.log(age);
