280e821a5fcdd5a495c3102fbf63acdfb010e92d
kid-us <kidushh29@gmail.com>
2024-04-16 12:12:53 +0300

    Fundamental Excerise

---
+++ hello_world/src/FundametalExercise.ts
@@ -0,0 +1,25 @@
+let a: number = 100;
+let b: string = "Tea";
+let c: boolean[] = [true, false, true];
+let d: {age: number} = {age: 1};
+let e: number[] = [3];
+let f: any;
+let g: any[] = []
+
+
+// Objects
+let song : {
+    title: string,
+    releaseYear: number
+} = {title: "My song", releaseYear: 2000};
+
+// Numbers Array
+let prices: number[] = [100, 200, 300];
+prices[0] = 50;
+
+// Function 
+function myFunc(a: number, b:number): number{
+    return a+b;
+}
+
+myFunc(2, 8);
