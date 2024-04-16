55536547fe7f897c7d9c4d4ad3873a2763875a6c
kid-us <kidushh29@gmail.com>
2024-04-16 19:46:02 +0300

    Type-Aliases

---
+++ hello_world/src/type-aliases.ts
@@ -0,0 +1,15 @@
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date:Date)=>void
+}
+
+let employee_info: Employee = {
+    id: 1,
+    name: "Mosh",
+    retire: (date:Date) =>
+        {
+            console.log(date);
+            
+        }
+}
