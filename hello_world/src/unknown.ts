1683a578701481da280bd62acc4519eb584fb505
kid-us <kidushh29@gmail.com>
2024-04-17 10:14:01 +0300

    unknown

---
+++ hello_world/src/unknown.ts
@@ -0,0 +1,8 @@
+function render (document : unknown) : string | number {
+    // Narrowing
+    if(typeof document === "string"){
+        return document.toLowerCase();
+    }else{
+        return 2;
+    }
+}
