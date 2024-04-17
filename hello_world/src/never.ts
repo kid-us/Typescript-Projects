e3a35d3881e5ef6b7265de8634ca8d0e73d81805
kid-us <kidushh29@gmail.com>
2024-04-17 10:20:40 +0300

    never

---
+++ hello_world/src/never.ts
@@ -0,0 +1,15 @@
+function render (message: string): never{
+    throw new Error (message);
+}
+
+
+function processEvents(): never {
+    while (true){
+        // Do something in here
+    }
+}
+
+processEvents();
+render('---');
+console.log("Hello World");
+
