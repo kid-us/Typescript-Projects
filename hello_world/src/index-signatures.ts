5ac26ff7e8699498ddec39c1e2a5d9c11553dbaf
kid-us <kidushh29@gmail.com>
2024-04-18 11:13:36 +0300

    Index Signatures

---
+++ hello_world/src/index-signatures.ts
@@ -0,0 +1,10 @@
+class SeatAssignment {
+    // A2, A2, ...
+    // Lorem, Ipsum, ...
+    // Index Signature properties
+    [seatNumber: string]: string;
+}
+
+let seats = new SeatAssignment();
+seats.A1 = "Lorem";
+seats.A2 = "Ipsum";
