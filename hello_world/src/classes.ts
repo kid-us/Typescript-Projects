1218a4a0782c136f1e8f1d0a3990fcd8195ccb39
kid-us <kidushh29@gmail.com>
2024-04-18 10:55:07 +0300

    Access Control keywords

--- hello_world/src/classes.ts
+++ hello_world/src/classes.ts
@@ -1,30 +1,35 @@
 // Creating Classes
-// class Account  {
-//     readonly id: number;
-//     owner: string;
-//     balance: number;
-//     nickname?: string;
+class Account  {
+    readonly id: number;
+    owner: string;
+    private _balance: number;
+    nickname?: string;
 
-//     constructor (id: number, owner: string, balance: number){
-//         this.id = id;
-//         this.owner = owner;
-//         this.balance = balance;
-//     }
+    constructor (id: number, owner: string, balance: number){
+        this.id = id;
+        this.owner = owner;
+        this._balance = balance;
+    }
 
-//     deposit(amount: number): void {
-//         if(amount <=0 ){
-//             throw new Error("Invalid Amount")
-//         }else{
-//             this.balance += amount
-//         }
+    deposit(amount: number): void {
+        if(amount <=0 ){
+            throw new Error("Invalid Amount")
+        }else{
+            this._balance += amount
+        }
+        this.calculateTax();
+    }
 
-//     }
-// }
+    getBalance(): number {
+        return this._balance;
+    }
 
-// // Creating object instance
+    private calculateTax () {
+    }
+}
 
-// const account = new Account(1, "Mosh", 0);
-// account.deposit(100);
+// Creating object instance
 
-// account.nickname = "Lorem";
-// console.log(account);
+const account = new Account(1, "Mosh", 100);
+account.nickname = "Lorem";
+console.log(account.getBalance());
--- hello_world/src/index.ts
+++ hello_world/src/index.ts
@@ -20,32 +20,32 @@
 
 // Creating Classes
 
-class Account  {
-    readonly id: number;
-    owner: string;
-    balance: number;
-    nickname?: string;
+// class Account  {
+//     readonly id: number;
+//     owner: string;
+//     balance: number;
+//     nickname?: string;
 
-    constructor (id: number, owner: string, balance: number){
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
-    }
+//     constructor (id: number, owner: string, balance: number){
+//         this.id = id;
+//         this.owner = owner;
+//         this.balance = balance;
+//     }
 
-    deposit(amount: number): void {
-        if(amount <=0 ){
-            throw new Error("Invalid Amount")
-        }else{
-            this.balance += amount
-        }
+//     deposit(amount: number): void {
+//         if(amount <=0 ){
+//             throw new Error("Invalid Amount")
+//         }else{
+//             this.balance += amount
+//         }
 
-    }
-}
+//     }
+// }
 
-// Creating object instance
+// // Creating object instance
 
-const account = new Account(1, "Mosh", 0);
-account.deposit(100);
+// const account = new Account(1, "Mosh", 0);
+// account.deposit(100);
 
-account.nickname = "Lorem";
-console.log(account);
+// account.nickname = "Lorem";
+// console.log(account);
