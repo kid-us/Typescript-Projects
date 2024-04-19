d47baf183b236b907f7ae6cf704c227aa2ccfdd3
kid-us <kidushh29@gmail.com>
2024-04-19 11:07:19 +0300

    Generic Functions

--- hello_world/src/generic-functions.ts
+++ hello_world/src/generic-functions.ts
@@ -1,5 +1,15 @@
-function wrapInArray (value: number){
+class ArrayUtils {
+    static wrapInArray<V> (value: V){
+        return [value];
+    }
+}
+
+let utils =  new ArrayUtils();
+let nums = ArrayUtils.wrapInArray(2)
+
+
+function wrapInArray<V> (value: V){
     return [value];
 }
 
-let arrNumbers = wrapInArray(2);
+let arrNumbers = wrapInArray("2");
