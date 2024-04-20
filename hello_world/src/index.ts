1ec101a6375bc6f80d427b3a8fe48646023182c5
kid-us <kidushh29@gmail.com>
2024-04-20 11:59:17 +0300

    Class Decorators

--- hello_world/src/index.ts
+++ hello_world/src/index.ts
@@ -1,79 +1,13 @@
-// let age: number = 20;
-// if (age < 50)
-//     age+=10;
-// console.log(age);
-
-// Unions Types
-
-// function kgToLbs (weight: number | string):number {
-//     // Narrowing 
-//     if (typeof weight === "number"){
-//         return weight * 2.2;
-//     }
-//     else{
-//         return parseInt(weight) * 2.2;
-//     }
-// }
-
-// console.log(kgToLbs("10kg"));
-// console.log(kgToLbs(10));
-class Person {
-    constructor (public firstName: string, public lastName: string){}
-
-    get fullName(){
-        return this.firstName + "  " + this.lastName;
-    }
-
-     walk() {
-        console.log("Ealking");
+function Component(constructor: Function){
+    console.log("Constructor decorator called");
+    constructor.prototype.uniqueId = Date.now();
+    constructor.prototype.insertInDom = () =>{
+        console.log("Inserting the component in the Dom");
     }
 }
 
-// Inherting the person class properties which is First Name and Last Name
-class Student extends Person {
-    constructor (public stundentId: number, firstName: string, lastName: string){
-        super(firstName, lastName);
-    }
-
-    getTest(){
-        console.log("Getting a Test");
-    }
-}
-
-// Method Overriding
-
-class Teacher extends Person {
-    override get fullName(){
-        return "Professor " + super.fullName;
-    }
-}
-// let student = new Student(1, "Lorem", "Ipsum");
-// let teacher = new Teacher ("Lorem", "Ipsum");
-// console.log(teacher.fullName);
-
-
-// Polymorphism
-
-class Principal extends Person {
-    override get fullName(){
-        return "Principal " + super.fullName;
-    }
-}
-
-printNames ([
-    new Student (1, "Lorem", "Ipsum"),
-    new Teacher ("Lorem", "Ipsum"),
-    new Principal ("Lorem", "Ipsum"),
-])
-
-// Polymorphism in action
-function printNames (people: Person[]){
-    for ( let person of people) {
-        console.log(person.fullName);
-        
-    }
-}
-
+@Component
+class ClassComponent{}
 
 
 
