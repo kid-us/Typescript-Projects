cd74791fc8cde4f316be892539097218b4fa391d
kid-us <kidushh29@gmail.com>
2024-04-22 17:04:42 +0300

    React App

---
+++ react-app/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Message from './components/Message'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Message />
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ react-app/src/components/Message.tsx
@@ -0,0 +1,10 @@
+
+const Message = () => {
+  return (
+    <div>
+        <h1>Message</h1>
+    </div>
+  )
+}
+
+export default Message
