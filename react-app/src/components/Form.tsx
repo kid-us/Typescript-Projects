e3b8157f59986cd6f897dec7ca83fa15b9862109
kid-us <kidushh29@gmail.com>
2024-04-26 18:50:43 +0300

    React Hook Form

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,47 +1,47 @@
-import { FormEvent, useState } from "react";
+// import { FormEvent, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form";
 
 export const Form = () => {
-  const [person, setPerson] = useState({
-    name: "",
-    age: "",
-  });
+  //   const [person, setPerson] = useState({
+  //     name: "",
+  //     age: "",
+  //   });
 
-  const handleSubmit = (event: FormEvent) => {
-    event.preventDefault();
-    console.log(person);
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
   };
+
   return (
-    <form onSubmit={handleSubmit}>
-      <div className="mb-2">
-        <label className="form-label" htmlFor="name">
-          Name
-        </label>
-        <input
-          onChange={(event) =>
-            setPerson({ ...person, name: event.target.value })
-          }
-          value={person.name}
-          id="name"
-          type="text"
-          className="form-control"
-        />
-      </div>
-      <div className="mb-2">
-        <label className="form-label" htmlFor="age">
-          Age
-        </label>
-        <input
-          onChange={(event) =>
-            setPerson({ ...person, age: event.target.value })
-          }
-          id="age"
-          type="number"
-          className="form-control"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-2">
+          <label className="form-label" htmlFor="name">
+            Name
+          </label>
+          <input
+            {...register("name")}
+            id="name"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-2">
+          <label className="form-label" htmlFor="age">
+            Age
+          </label>
+          <input
+            {...register("age")}
+            id="age"
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </>
   );
 };
