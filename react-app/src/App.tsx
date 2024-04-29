657b5e4b5342221b175017bbd77ed4cdebe4765e
kid-us <kidushh29@gmail.com>
2024-04-29 10:46:57 +0300

    Expense Tracker, Expense Lists

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./App.css";
-import { ExpenseTracker } from "./components/ExpenseTracker";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 100, category: "Lorem" },
+    { id: 2, description: "bbb", amount: 100, category: "Lorem" },
+    { id: 3, description: "ccc", amount: 100, category: "Lorem" },
+    { id: 4, description: "ddd", amount: 100, category: "Lorem" },
+    { id: 5, description: "eee", amount: 100, category: "Lorem" },
+  ]);
+
+  const handleDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
+
   return (
     <>
       <div className="p-5">
-        <ExpenseTracker />
+        <ExpenseList expense={expenses} onDelete={(id) => handleDelete(id)} />
       </div>
     </>
   );
