74b11775ab2bfafbefe4244324f14f932acad468
kid-us <kidushh29@gmail.com>
2024-05-01 07:56:43 +0300

    useEffect

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,22 @@
+import { useEffect, useRef } from "react";
 import "./App.css";
-import Expense from "./expense-tracker/components/Expesnse";
+// import Expense from "./expense-tracker/components/Expesnse";
 function App() {
+  const focusRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (focusRef.current) focusRef.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
+
   return (
     <>
-      <div className="container">
+      <div className="container mt-5">
         {/* <Expense /> */}
+        <input ref={focusRef} type="text" className="form-control" />
       </div>
     </>
   );
