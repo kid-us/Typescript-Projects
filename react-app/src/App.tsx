7fd75b2562fab2c4dceb2de3a671506e848f6871
kid-us <kidushh29@gmail.com>
2024-05-02 08:34:25 +0300

    Fetching Data

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,18 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+// import ProductList from "./components/ProductList";
 // import Expense from "./expense-tracker/components/Expesnse";
+interface User {
+  id: number;
+  name: string;
+}
 function App() {
   // const focusRef = useRef<HTMLInputElement>(null);
 
-  const [category, setCategory] = useState("");
+  const [users, setUsers] = useState<User[]>([]);
+
+  // const [category, setCategory] = useState("");
 
   // useEffect(() => {
   //   if (focusRef.current) focusRef.current.focus();
   // });
 
   useEffect(() => {
-    document.title = "My App";
+    // document.title = "My App";
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((response) => console.log(setUsers(response.data)));
   });
 
   return (
@@ -20,7 +30,7 @@ function App() {
       <div className="container mt-5">
         {/* <Expense /> */}
         {/* <input ref={focusRef} type="text" className="form-control" /> */}
-        <label htmlFor="category" className="form-label">
+        {/* <label htmlFor="category" className="form-label">
           Category
         </label>
         <select
@@ -32,7 +42,14 @@ function App() {
           <option value="Lorem">Lorem</option>
           <option value="Ipsum">Ipsum</option>
         </select>
-        <ProductList category={category} />
+        <ProductList category={category} /> */}
+
+        {/* Fetching Data */}
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
       </div>
     </>
   );
